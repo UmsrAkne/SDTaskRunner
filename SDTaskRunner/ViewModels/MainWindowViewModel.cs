c868c47e740602eeb16b3a29e9cbd40b9051db65
UmsrAkne <sibata486@gmail.com>
2025-12-26 01:06:38 +0900

    feat: Bind `GenerationRequest` to MainWindowViewModel and update UI for data entry

--- SDTaskRunner/ViewModels/MainWindowViewModel.cs
+++ SDTaskRunner/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Mvvm;
+﻿using System.Diagnostics;
+using Prism.Mvvm;
+using SDTaskRunner.Models;
 using SDTaskRunner.Utils;
 
 namespace SDTaskRunner.ViewModels;
@@ -6,6 +8,31 @@ namespace SDTaskRunner.ViewModels;
 public class MainWindowViewModel : BindableBase
 {
     private readonly AppVersionInfo appVersionInfo = new();
+    private GenerationRequest generationRequest;
+
+    public MainWindowViewModel()
+    {
+        SetDummies();
+    }
 
     public string Title => appVersionInfo.GetAppNameWithVersion();
+
+    public GenerationRequest GenerationRequest
+    {
+        get => generationRequest;
+        set => SetProperty(ref generationRequest, value);
+    }
+
+    [Conditional("DEBUG")]
+    private void SetDummies()
+    {
+        GenerationRequest = new();
+
+        GenerationRequest.Height.Value = 512;
+        GenerationRequest.Width.Value = 480;
+        GenerationRequest.Steps.Value = 10;
+        GenerationRequest.Prompt.Value = "A painting of a squirrel eating a burger";
+        GenerationRequest.NegativePrompt.Value = "Negative prompt test.";
+        GenerationRequest.Seed.Value = 1234;
+    }
 }
